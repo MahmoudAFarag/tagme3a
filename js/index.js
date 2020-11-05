ec439e08d9a46ac68c9ac4e33a70c473eaa7c76a
Mahmoud Ashraf <mahmoudashrafsaad8@gmail.com>
2020-11-05 21:59:43 +0200

    bypass popup blocker

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const selection1 = document.querySelector(".week__material-sectionDropDown");
 selection1.addEventListener("change", ({ target: { value } }) => {
   if (value === "choose") return false;
-  window.open(value, "_blank");
+  let newTab = window.open();
+  newTab.location.href = value;
 });
