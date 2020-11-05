d18d6021dd8382998130539d2cd1994229197df2
Mahmoud Ashraf <mahmoudashrafsaad8@gmail.com>
2020-11-05 22:14:25 +0200

    remove external javascript

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const selection1 = document.querySelector(".week__material-sectionDropDown");
-selection1.addEventListener("change", ({ target: { value } }) => {
-  if (value === "choose") return false;
-  let newTab = window.open();
-  newTab.location.href = value;
-});
